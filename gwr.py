b11cff10de75e59736cf4ad7e81d996cf307997b
drawlinson <dave@agi.io>
2020-06-28 17:08:19 +1000

    1. Only grow up to a finite size 2. Make hyperparam names easier to remember

--- gwr.py
+++ gwr.py
@@ -30,14 +30,17 @@ class GrowingWhenRequired:
         
     @staticmethod
     def default_hparams():
+        """Hyperparameter input_shape must be 4d for compatibility with other conv ANN. The dimensions are batch_size, h, w, channels or depth."""
         hparams = {
-            'num_inputs': 0, 
-            'e_b': 0.1, 
-            'e_n': 0.006, 
-            'a_max': 10,
-            'l': 200, 
-            'a': 0.5, 
-            'd': 0.995
+            'input_shape':None,
+            'init_range':2.0,
+            'learning_rate_best': 0.1,
+            'learning_rate_neighbour': 0.006,
+            'max_age': 10,
+            'max_size':-1,
+            'growth_interval': 200,
+            'error_split': 0.5,
+            'error_decay': 0.995
         }
         return hparams
 
@@ -51,13 +54,21 @@ class GrowingWhenRequired:
         ranking = [u for u, dist in distance]
         return ranking
 
+    def distance(self, vector, observation):
+        dist = spatial.distance.euclidean(vector, observation)
+        return dist
+
     def prune_connections(self, a_max):
         listToRemoveE = []
         listToRemoveU = []
+
+        # Remove edges that haven't been used for a while
         for u, v, attributes in self.network.edges(data=True):
             if attributes['age'] > a_max:
                 #self.network.remove_edge(u, v)
                 listToRemoveE.append([u, v])
+
+        # Remove nodes with no edges
         for u in self.network.nodes():
             if self.network.degree(u) == 0:
                 #self.network.remove_node(u)
@@ -90,8 +101,10 @@ class GrowingWhenRequired:
 
     def init(self, ):
         #w_shape = np.shape(self.data)[1]
-        num_inputs = self.hparams['num_inputs']
-        w = [np.random.uniform(-2, 2) for _ in range(num_inputs)]
+        input_shape = self.hparams['input_shape']
+        num_inputs = np.prod(input_shape)
+        init_range = self.hparams['init_range']
+        w = [np.random.uniform(-init_range, init_range) for _ in range(num_inputs)]
         return w
         
     def reset(self, ):
@@ -107,12 +120,13 @@ class GrowingWhenRequired:
         
     def update(self, observation, step):
         # Get hyperparameters
-        e_b = self.hparams['e_b']
-        e_n = self.hparams['e_n']
-        a_max = self.hparams['a_max']
-        l = self.hparams['l']
-        a = self.hparams['a']
-        d = self.hparams['d']
+        e_b = self.hparams['learning_rate_best']
+        e_n = self.hparams['learning_rate_neighbour']
+        max_age = self.hparams['max_age']
+        max_size = self.hparams['max_size']
+        l = self.hparams['growth_interval']
+        a = self.hparams['error_split']
+        #d = self.hparams['error_decay']
         
         # 2. find the nearest unit s_1 and the second nearest unit s_2
         nearest_units = self.find_nearest_units(observation)
@@ -123,7 +137,9 @@ class GrowingWhenRequired:
         for u, v, attributes in self.network.edges(data=True, nbunch=[s_1]):                    
             self.network.add_edge(u, v, age=attributes['age']+1)
         # 4. add the squared distance between the observation and the nearest unit in input space
-        dist = spatial.distance.euclidean(observation, self.network.node[s_1]['vector'])**2
+        #dist = spatial.distance.euclidean(observation, self.network.node[s_1]['vector'])**2
+        node = self.network.node[s_1]['vector']
+        dist = self.distance(observation, node) **2
         self.network.node[s_1]['error'] += dist
         # 5 .move s_1 and its direct topological neighbors towards the observation by the fractions
         #    e_b and e_n, respectively, of the total distance
@@ -135,44 +151,46 @@ class GrowingWhenRequired:
         # 6. if s_1 and s_2 are connected by an edge, set the age of this edge to zero
         #    if such an edge doesn't exist, create it
         self.network.add_edge(s_1, s_2, age=0)
-        # 7. remove edges with an age larger than a_max
+        # 7. remove edges with an age larger than max_age
         #    if this results in units having no emanating edges, remove them as well
-        self.prune_connections(a_max)
+        self.prune_connections(max_age)
         # 8. if the number of steps so far is an integer multiple of parameter l, insert a new unit
         step += 1
         if step % l == 0:
-            #if plot_evolution:
-            #    self.plot_network(data, 'visualization/sequence/' + str(sequence) + '.png')
-            #sequence += 1
-            # 8.a determine the unit q with the maximum accumulated error
-            q = 0
-            error_max = 0
-            #for u in self.network.nodes_iter():
-            for u in self.network.nodes():                        
-                if self.network.node[u]['error'] > error_max:
-                    error_max = self.network.node[u]['error']
-                    q = u
-            # 8.b insert a new unit r halfway between q and its neighbor f with the largest error variable
-            f = -1
-            largest_error = -1
-            for u in self.network.neighbors(q):
-                if self.network.node[u]['error'] > largest_error:
-                    largest_error = self.network.node[u]['error']
-                    f = u
-            w_r = 0.5 * (np.add(self.network.node[q]['vector'], self.network.node[f]['vector']))
-            r = self.units_created
-            self.units_created += 1
-            # 8.c insert edges connecting the new unit r with q and f
-            #     remove the original edge between q and f
-            self.network.add_node(r, vector=w_r, error=0)
-            self.network.add_edge(r, q, age=0)
-            self.network.add_edge(r, f, age=0)
-            self.network.remove_edge(q, f)
-            # 8.d decrease the error variables of q and f by multiplying them with a
-            #     initialize the error variable of r with the new value of the error variable of q
-            self.network.node[q]['error'] *= a
-            self.network.node[f]['error'] *= a
-            self.network.node[r]['error'] = self.network.node[q]['error']
+            num_nodes = len(self.network.nodes())
+            if (max_size < 0) or (num_nodes < max_size):
+                #if plot_evolution:
+                #    self.plot_network(data, 'visualization/sequence/' + str(sequence) + '.png')
+                #sequence += 1
+                # 8.a determine the unit q with the maximum accumulated error
+                q = 0
+                error_max = 0
+                #for u in self.network.nodes_iter():
+                for u in self.network.nodes():
+                    if self.network.node[u]['error'] > error_max:
+                        error_max = self.network.node[u]['error']
+                        q = u
+                # 8.b insert a new unit r halfway between q and its neighbor f with the largest error variable
+                f = -1
+                largest_error = -1
+                for u in self.network.neighbors(q):
+                    if self.network.node[u]['error'] > largest_error:
+                        largest_error = self.network.node[u]['error']
+                        f = u
+                w_r = 0.5 * (np.add(self.network.node[q]['vector'], self.network.node[f]['vector']))
+                r = self.units_created
+                self.units_created += 1
+                # 8.c insert edges connecting the new unit r with q and f
+                #     remove the original edge between q and f
+                self.network.add_node(r, vector=w_r, error=0)
+                self.network.add_edge(r, q, age=0)
+                self.network.add_edge(r, f, age=0)
+                self.network.remove_edge(q, f)
+                # 8.d decrease the error variables of q and f by multiplying them with a
+                #     initialize the error variable of r with the new value of the error variable of q
+                self.network.node[q]['error'] *= a
+                self.network.node[f]['error'] *= a
+                self.network.node[r]['error'] = self.network.node[q]['error']
         # 9. decrease all error variables by multiplying them with a constant d
         error = 0
         for u in self.network.nodes():
@@ -193,13 +211,15 @@ class GrowingWhenRequired:
             for observation in data:
                 nearest_units = gng.find_nearest_units(observation)
                 s_1 = nearest_units[0]
-                # TODO make the distance fn reusable
-                global_error += spatial.distance.euclidean(observation, gng.network.node[s_1]['vector'])**2
+                #global_error += spatial.distance.euclidean(observation, gng.network.node[s_1]['vector'])**2
+                node = gng.network.node[s_1]['vector']
+                dist = self.distance(observation, node)**2
+                global_error += dist
             return global_error
         
         
-        l = self.hparams['l']
-        d = self.hparams['d']
+        l = self.hparams['growth_interval']
+        d = self.hparams['error_decay']
         
         self.reset()
         #self.units_created = 0
